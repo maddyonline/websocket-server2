d482857af0d1bc782caa28e1f63b5011757da61a
Madhav Jha <madhavjha@gmail.com>
2022-04-04 09:14:28 -0700

    more

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer } from "ws";
+import admin from "firebase-admin";
+import serviceAccount from "./serviceAccount.js";
+import { getFirestore } from "firebase-admin/firestore";
 
-const wss = new WebSocketServer({ port: 1323 });
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
-
-  ws.send('something');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
+const wss = new WebSocketServer({ port: 1323 });
+const db = getFirestore();
+console.log(db);
+const docRef = db.collection("heart-rates").doc("maddy");
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", async function message(data) {
+    console.log("received: %s", data);
+    await docRef.set({
+      data,
+      from: "server",
+    });
+  });
+
+  ws.send("something");
+});
